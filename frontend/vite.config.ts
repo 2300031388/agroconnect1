480cedba35dd8a9019bd877400f0882b460ba05d
2300031388 <2300031388@kluniversity.in>
2025-09-27 13:48:39 +0530

    Initial commit for agroconnect1

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -3,7 +3,7 @@ import react from '@vitejs/plugin-react';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: 'agroconnect',
+  base: '/agroconnect1/',
   plugins: [react()],
   optimizeDeps: {
     exclude: ['lucide-react'],
